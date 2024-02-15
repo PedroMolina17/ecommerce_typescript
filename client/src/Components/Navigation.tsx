0f5a3c53ca0e1f71a4e35ba1f906bd3f3a09f0df
pedromolina17 <pedro.molinanoa@gmail.com>
2024-02-15 18:30:40 -0500

    Config

---
+++ client/src/Components/Navigation.tsx
@@ -0,0 +1,14 @@
+const Navigation = () => {
+  return (
+    <div className="flex bg-[#129cb9]">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
