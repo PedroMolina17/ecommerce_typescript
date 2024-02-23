bb44ddc95b7f231666a47c572b82b204717b0106
santiagoweb21212 <juanusca45@gmail.com>
2024-02-23 08:42:06 -0500

    add service for upload img

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -2,22 +2,29 @@ import { PrismaClient } from "@prisma/client";
 import { IProductCreate, IProductDelete, IProductUpdate } from "../types/product.type";
 import ClientError from "../errors/clientError.error";
 import { HTTP_STATUS } from "../constants/statusCode.constants";
+import { CloudinaryService } from "./cloudinary/cloudinary.service";
+import fs from 'fs-extra'
 const prisma = new PrismaClient()
 export class ProductsService {
     // Add your product-related service methods here
-    static async createProduct(product:IProductCreate) {
+    static async createProduct(product: IProductCreate) {
         // Logic to create a new product
         const existsProduct = await prisma.products.findUnique({
             where: {
                 name: product.name
             }
         })
-        if(existsProduct){
+
+        if (existsProduct) {
             throw new ClientError("Product already exists", HTTP_STATUS.NOT_FOUND)
         }
-        if(product.image){
-             console.log("upload Image")
-        }
+
+        if (!product.image) throw new ClientError("image is required", HTTP_STATUS.BAD_REQUEST)
+
+        const result = await CloudinaryService.uploadImg(product.image,undefined)
+        fs.unlink(product.image)
+        product.image = result.secure_url
+
         const newProduct = await prisma.products.create({
             data: {
                 ...product
@@ -31,16 +38,16 @@ export class ProductsService {
 
     }
 
-    static async updateProduct(product:IProductUpdate) {
+    static async updateProduct(product: IProductUpdate) {
         const existsProduct = await prisma.products.findUnique({
             where: {
                 id: product.id
             }
         })
-        if(!existsProduct){
+        if (!existsProduct) {
             throw new ClientError("Product not found", HTTP_STATUS.NOT_FOUND)
         }
-        if(product.image){
+        if (product.image) {
             console.log("actualizando imagen")
         }
         const updatedProduct = await prisma.products.update({
@@ -56,13 +63,13 @@ export class ProductsService {
             message: "Product updated"
         }
     }
-    static async deleteProduct(product:IProductDelete) {
+    static async deleteProduct(product: IProductDelete) {
         const existsProduct = await prisma.products.findUnique({
             where: {
                 id: product.id
             }
         })
-        if(!existsProduct){
+        if (!existsProduct) {
             throw new ClientError("Product not found", HTTP_STATUS.NOT_FOUND)
         }
         const deletedProduct = await prisma.products.delete({
