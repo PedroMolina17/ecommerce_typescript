541fba3a3143c69b0bacc6e97a44d25212ba34cc
santiagoweb21212 <juanusca45@gmail.com>
2024-02-23 08:41:37 -0500

    add midleware for route /create-product

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,8 +1,8 @@
-import {Router} from "express"
+import { Router } from "express"
 import { createProduct, deleteProduct, updateProduct } from "../controllers/admin/products.ctrl"
+import { upload } from "../configs/multer.config"
 const router = Router()
-
-router.post("/create-product", createProduct)
+router.post("/create-product", upload.single("image"), createProduct)
 router.put("/update-product", updateProduct)
 router.delete("/delete-product", deleteProduct)
 
