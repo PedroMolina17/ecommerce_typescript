c9ac86e62cb2fd6c10a16737f80ad58d141980ee
santiagoweb21212 <juanusca45@gmail.com>
2024-03-05 19:26:46 -0500

    update: -protecion a rutas -config axios -migrations -se agrego midlewares de vericacion  de role y verifcacion de token

--- client/src/api/axios.config.ts
+++ client/src/api/axios.config.ts
@@ -2,4 +2,5 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:3500/api",
+  withCredentials: true,
 });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import router from "./routes";
 import morgan from "morgan";
-/* import cokieParser from "cookie-parser"; */
+import cookieParser from "cookie-parser";
 import { sendErrorResponse } from "./utils/sendErrorResponse.util";
 export const ENV =
   process.env.NODE_ENV === "DEVELOPMENT"
@@ -19,9 +19,12 @@ export const ENV =
 const app = express();
 
 app.use(express.json()); 
-/* app.use(cokieParser()); */
+app.use(cookieParser());
 app.use(morgan("dev")); 
-app.use(cors()); 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+})); 
 
 app.use("/api", router);
 
