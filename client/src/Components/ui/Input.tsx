869784df2e548ab7217e9cd6d2937a0ebdedf548
BryanD <58919396+5ylver@users.noreply.github.com>
2024-04-06 17:27:38 -0500

    Socket client (#51)
    * feat(Admin): added edit UserProfile
    
    * feat: WebSocket client configuration

--- client/src/Components/ui/Input.tsx
+++ client/src/Components/ui/Input.tsx
@@ -8,14 +8,35 @@ interface InputProps
   labelText?: string;
   labelFor?: string;
   register?: any;
-  labelClass?:string
+  labelClass?: string;
+  error?: boolean;
 }
-const Input = ({ labelText, labelClass,labelFor, register,...props }: InputProps) => {
+
+const Input = ({
+  labelText,
+  labelClass,
+  labelFor,
+  register,
+  error,
+  className,
+  ...props
+}: InputProps) => {
   return (
     <div className="flex flex-col gap-0.5">
-      <label htmlFor={labelFor} className={`${labelClass}`}>{labelText}</label>
-      <input {...props} {...register} />
+      <label htmlFor={labelFor} className={`${labelClass}`}>
+        {labelText}
+      </label>
+      <input
+        {...props}
+        className={`${className} ${
+          error
+            ? "focus:border-red-300 focus:ring-red-300"
+            : "focus:ring-blue-300 focus:border-blue-300"
+        } `}
+        {...register}
+      />
     </div>
   );
 };
+
 export default Input;
