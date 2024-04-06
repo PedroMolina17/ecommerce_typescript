774a3d9e6e4afa8a807d7f515bac797deae8bf03
BryanD <58919396+5ylver@users.noreply.github.com>
2024-04-06 17:27:38 -0500

    Socket client (#51)
    * feat(Admin): added edit UserProfile
    
    * feat: WebSocket client configuration

--- client/src/Components/Admin/Navbar/NavBar.tsx
+++ client/src/Components/Admin/Navbar/NavBar.tsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+
+import io from "socket.io-client";
 import { Notifications, Settings, UserProfile } from "./components";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
+  // const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3500");
+
+    socket.on("connect", () => {
+      console.log("Connection established.");
+    });
+
+    socket.on("message", (data) => {
+      console.log("Message:", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Closed connection.");
+    });
+  }, []);
+
   return (
     <nav className="bg-secondary fixed h-16 z-30 top-0 right-0 left-0 border-b border-gray-700 text-white flex items-center justify-end pr-10 space-x-4">
       <Notifications />
--- client/src/Components/Admin/login/Login.tsx
+++ client/src/Components/Admin/login/Login.tsx
@@ -1,97 +1,100 @@
-import React, { useState } from "react";
+import { useEffect } from "react";
 
-import { ToastContainer, toast } from "react-toastify";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
-import Logo from "@/assets/logo.jpeg";
-import { Input } from "./Input";
-import { ILogin } from "@/types/auth.type";
 import { login } from "@/api/auth";
+import Logo from "@/assets/logo.jpeg";
+import Input from "@ui/Input";
+
+interface FormValue {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  const [valueForm, setValueForm] = useState<ILogin>({
-    email: "admin@example.com",
-    password: "password",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setFocus,
+  } = useForm<FormValue>({
+    defaultValues: { email: "admin@example.com", password: "password" },
   });
 
   const navigate = useNavigate();
 
-  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const { value, name } = e.currentTarget;
+  const onSubmit = handleSubmit(async (values) => {
+    await login(values);
+    navigate("/dashboard");
+  });
 
-    setValueForm({
-      ...valueForm,
-      [name]: value,
-    });
-  };
-
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      if (valueForm.email.length == 0) {
-        toast.error("Escriba un correo", {
-          position: "top-center",
-        });
-      }
-      if (valueForm.password.length == 0) {
-        toast.error("Escriba una contraseña", {
-          position: "top-center",
-        });
-      }
-
-      const response = await login(valueForm);
-      console.log(response);
-
-      if (response.status == 200 && response.statusText == "OK") {
-        toast.success("Iniciando sesión con éxito!", {
-          position: "top-center",
-        });
-        navigate("/dashboard");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    setFocus("email");
+  }, [setFocus]);
 
   return (
-    <section className="bg-gray-50 min-h-screen flex items-center justify-center w-full ">
-      <ToastContainer />
-      <div className="bg-[#455591] flex rounded-[50px] shadow-lg p-5 items-center">
-        <div className="md:w-1/2 px-16 mr-2 -mt-12">
-          <h2 className="text-white text-3xl">Iniciar Sesión</h2>
-          <p className="text-sm mt-4 text-white">
-            Si eres administrador, porfavor inicie sesión
-          </p>
-          <form onSubmit={handleOnSubmit} className="flex flex-col gap-4 mt-14">
-            <Input
-              placeholder="Escriba su correo"
-              type="email"
-              name="email"
-              onChange={handleOnChange}
-              value={valueForm.email}
-            />
-            <Input
-              placeholder="Escriba su contraseña"
-              type="password"
-              name="password"
-              onChange={handleOnChange}
-              value={valueForm.password}
-            />
-            <button
-              className="bg-[#f97f63] rounded-3xl text-white py-2 mt-10 hover:bg-[#e6917e] hover:scale-105 duration-300"
-              type="submit"
-            >
-              Iniciar
-            </button>
-          </form>
+    <>
+      <section className="bg-bg min-h-screen flex items-center justify-center w-full">
+        <div className="bg-[#455591] flex rounded-2xl shadow-lg items-center overflow-hidden">
+          <div className="md:w-1/2 px-16 mr-4">
+            <h2 className="text-white text-3xl text-center font-bold">
+              Sign In
+            </h2>
+            <p className="text-sm mt-4 text-white text-center">
+              Get access to your account
+            </p>
+            <form onSubmit={onSubmit} className="flex flex-col mt-5">
+              <Input
+                type="email"
+                className="bg-gray-50 border border-gray-400 rounded-md block w-full py-1 px-2 outline-none focus:ring-2"
+                labelText="Email"
+                labelClass="text-white text-sm"
+                placeholder="Email"
+                register={register("email", {
+                  required: {
+                    value: true,
+                    message: "Please fill out this field",
+                  },
+                })}
+                error={Boolean(errors?.email?.message)}
+              />
+              {errors.email && (
+                <span className="text-white">{errors.email.message}</span>
+              )}
+
+              <Input
+                type="password"
+                className="bg-gray-50 border border-gray-400 rounded-md block w-full py-1 px-2 outline-none focus:ring-2"
+                labelText="Password"
+                labelClass="text-white text-sm mt-4"
+                register={register("password", {
+                  required: {
+                    value: true,
+                    message: "Please fill out this field",
+                  },
+                })}
+                error={Boolean(errors?.password?.message)}
+              />
+              {errors.password && (
+                <span className="text-white">{errors.password.message}</span>
+              )}
+
+              <button
+                className="bg-[#f97f63] rounded-3xl text-white py-2 mt-10 hover:bg-[#e6917e] hover:scale-105 duration-300"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </form>
+          </div>
+          <div className="md:block hidden">
+            <img src={Logo} alt="" className="" />
+          </div>
         </div>
-        <div className="md:block w-96 hidden rounded-3xl">
-          <img src={Logo} alt="" className="rounded-2xl" />
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
