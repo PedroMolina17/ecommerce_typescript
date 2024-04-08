b6d5d97af6bbddea5dbf126134f38a397e5e4661
pedromolina17 <52333132+PedroMolina17@users.noreply.github.com>
2024-04-08 17:39:02 -0500

    Frontend (#52)
    * colors tailwind
    
    * Sales
    
    * Circle Graph
    
    * Tailwind
    
    * Design Product , and colors tailwind fix

--- client/src/Components/Admin/sideBar/Sidebar.tsx
+++ client/src/Components/Admin/sideBar/Sidebar.tsx
@@ -3,11 +3,9 @@ import { FaGithub, FaUser } from "react-icons/fa";
 import { FaCartShopping, FaDollarSign } from "react-icons/fa6";
 import { SiBrandfolder } from "react-icons/si";
 import { TbCategoryFilled } from "react-icons/tb";
-
 import ButonSideBar from "./ButonSideBar";
 import ContainerButton from "./ContainerButton";
 import { useSelectNavStore } from "./store/useSelectNav";
-import logo from "/images/logo-Celeste.png";
 
 const Sidebar = ({ isOpen, setIsOpen }: any) => {
   const { setSelectNav } = useSelectNavStore((state) => state);
@@ -15,34 +13,29 @@ const Sidebar = ({ isOpen, setIsOpen }: any) => {
   return (
     <aside
       className={`${
-        isOpen ? "w-64" : "w-16"
-      } duration-150 fixed z-40 bg-secondary col-span-2 text-white row-span-12 left-0 top-0  min-h-screen border-r border-gray-700`}
+        isOpen ? "w-80" : "w-12"
+      } duration-150 fixed z-40 bg-primary col-span-2 text-darkText row-span-12 rounded-tr-3xl rounded-br-3xl   left-0 top-0  min-h-screen  py-8 pl-4 font-bold`}
     >
       <BsArrowLeftShort
         className={`${
           !isOpen && "rotate-180"
-        } text-pretty text-[24px] bg-white absolute -right-3 top-5 text-[#139dba] border border-[#139dba] rounded-full cursor-pointer`}
+        } text-pretty text-[24px] bg-darkPrimary absolute -right-3 top-5   rounded-full cursor-pointer  `}
         onClick={() => setIsOpen(!isOpen)}
       />
       <div className=" h-full w-full flex flex-col gap-1">
         <div className="h-12 overflow-hidden flex items-center gap-1 mb-6">
-          <img
-            src={logo}
-            className={`w-16 h-12 object-contain bg-white`}
-            alt="logo"
-          />
-          <h2 className="text-primary text-lg whitespace-nowrap  text-ellipsis font-medium ">
+          <h2 className=" text-lg whitespace-nowrap  text-ellipsis font-medium ">
             Bienvenido a Celeste
           </h2>
         </div>
 
         <ContainerButton
-          title="costo y presupuestos"
+          title="Costo y presupuestos"
           name="Sales"
           isOpen={isOpen}
           onClick={() => setSelectNav("Sales")}
         >
-          <ButonSideBar icon={<FaDollarSign className="text-primary" />} />
+          <ButonSideBar icon={<FaDollarSign className="text-darkText" />} />
         </ContainerButton>
         <ContainerButton
           title="Customers"
@@ -50,7 +43,7 @@ const Sidebar = ({ isOpen, setIsOpen }: any) => {
           isOpen={isOpen}
           onClick={() => setSelectNav("Users")}
         >
-          <ButonSideBar icon={<FaUser className="text-primary" />} />
+          <ButonSideBar icon={<FaUser className="text-darkText" />} />
         </ContainerButton>
         <ContainerButton
           title="Categories"
@@ -58,7 +51,7 @@ const Sidebar = ({ isOpen, setIsOpen }: any) => {
           isOpen={isOpen}
           onClick={() => setSelectNav("Categories")}
         >
-          <ButonSideBar icon={<TbCategoryFilled className="text-primary" />} />
+          <ButonSideBar icon={<TbCategoryFilled className="text-darkText" />} />
         </ContainerButton>
         <ContainerButton
           title="Products"
@@ -66,7 +59,7 @@ const Sidebar = ({ isOpen, setIsOpen }: any) => {
           isOpen={isOpen}
           onClick={() => setSelectNav("Products")}
         >
-          <ButonSideBar icon={<FaCartShopping className="text-primary" />} />
+          <ButonSideBar icon={<FaCartShopping className="text-darkText" />} />
         </ContainerButton>
 
         <ContainerButton
@@ -75,7 +68,7 @@ const Sidebar = ({ isOpen, setIsOpen }: any) => {
           isOpen={isOpen}
           onClick={() => setSelectNav("Brands")}
         >
-          <ButonSideBar icon={<SiBrandfolder className="text-primary" />} />
+          <ButonSideBar icon={<SiBrandfolder className="text-darkText" />} />
         </ContainerButton>
 
         <a
