cbf854d7bea53ed5029319ea14eaf53407a6e1b7
pedromolina17 <pedro.molinanoa@gmail.com>
2024-04-14 20:10:37 -0500

    Create Product Complete

--- client/src/Components/Admin/Navbar/NavBar.tsx
+++ client/src/Components/Admin/Navbar/NavBar.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import io from "socket.io-client";
 import { Notifications, Settings, UserProfile } from "./components";
-
+import { MdLightMode } from "react-icons/md";
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
   // const [socket, setSocket] = useState<Socket | null>(null);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   useEffect(() => {
     const socket = io("http://localhost:3500");
@@ -25,7 +30,8 @@ const NavBar: React.FC<NavBarProps> = () => {
   }, []);
 
   return (
-    <nav className="bg-secondary fixed h-16 z-30 top-0 right-0 left-0 border-b border-gray-700 text-white flex items-center justify-end pr-10 space-x-4">
+    <nav className="text-4xl bg-dark-secondary fixed h-16 z-30 top-0 right-0 left-0 border-b border-gray-700 text-white flex items-center justify-end pr-10 space-x-4">
+      <MdLightMode onClick={toggleDarkMode} />
       <Notifications />
       <Settings />
       <UserProfile />
--- client/src/Components/Admin/Products/CreateProduct.tsx
+++ client/src/Components/Admin/Products/CreateProduct.tsx
@@ -1,4 +1,10 @@
 import { FaImage } from "react-icons/fa";
+import {
+  MdOutlineAttachMoney,
+  MdOutlineProductionQuantityLimits,
+  MdLocalOffer,
+  MdOutlineLocalOffer,
+} from "react-icons/md";
 import { useRef, useState } from "react";
 import { getAllCategory } from "../../../api/category";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
@@ -16,6 +22,22 @@ const CreateProduct = () => {
   const { handleSubmit, register, reset } = useForm<FormularioProductoProps>();
   const { setOpenForm } = useOpenFormStoreProduct();
   const queryClient = useQueryClient();
+
+  interface FormularioProductoProps {
+    name: string;
+    categoryId: number;
+    price: number;
+    image: string | null;
+    categories: { id: number; name: string }[];
+    description: string;
+    stock: number;
+    status: boolean;
+    promotion: boolean;
+    promotionPrice: number;
+    promotionDescription?: string;
+    brandId: number;
+    brands: { id: number; name: string }[];
+  }
 
   //Crear Categoria
   const createProductMutation = useMutation({
@@ -63,8 +85,6 @@ const CreateProduct = () => {
     }
     formData.append("description", data.description);
     formData.append("stock", data.stock.toString());
-    formData.append("status", data.status.toString());
-    formData.append("promotion", data.promotion.toString());
     formData.append("promotionPrice", data.promotionPrice.toString());
     formData.append("promotionDescription", data.promotionDescription || "");
     formData.append("brandId", data.brandId.toString());
@@ -74,13 +94,14 @@ const CreateProduct = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex text-darkText flex-col">
+      <div className="flex text-darkSecondary flex-col">
         <div className="flex justify-between items-center">
           <h1 className="text-4xl block my-4">Add Product</h1>
           <div className="flex gap-4 items-center justify-center font-bold">
             <button
               className="bg-blue-600 p-6  h-8 rounded-md flex items-center "
               type="submit"
+              onClick={() => setOpenForm("create")}
             >
               Agregar
             </button>
@@ -94,16 +115,15 @@ const CreateProduct = () => {
           <div className="grid col-span-8">
             <div className="flex flex-col gap-8">
               <label className="flex flex-col gap-2">
-                Producto
+                <p className="text-xl">Producto</p>
                 <input
                   type="text"
                   {...register("name", { required: "Campo obligatorio" })}
                   className=" bg-darkSecondary rounded-sm h-8 text-darkPrimary p-1"
                 />
               </label>
-
               <label className="flex flex-col gap-2">
-                Producto
+                <p className="text-xl">Descripcion</p>
                 <textarea
                   {...register("description", {
                     required: "Campo obligatorio",
@@ -111,6 +131,59 @@ const CreateProduct = () => {
                   className=" bg-darkSecondary rounded-sm h-48 text-darkPrimary p-1"
                 />
               </label>
+              <div className="flex flex-col gap-2">
+                <p className="text-xl">Inventario</p>
+                <div className="border p-4 flex flex-col gap-4 text-lg">
+                  <label className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MdOutlineAttachMoney /> Precio
+                    </div>
+                    <input
+                      {...register("price", { required: "Campo obligatorio" })}
+                      type="number"
+                      className="w-96 rounded-sm text-black p-1"
+                      placeholder="$0.00"
+                    />
+                  </label>
+                  <label className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MdOutlineProductionQuantityLimits /> Cantidad
+                    </div>
+                    <input
+                      {...register("stock", { required: "Campo obligatorio" })}
+                      type="number"
+                      className="w-96 rounded-sm text-black p-1"
+                      placeholder="0"
+                    />
+                  </label>
+                  <label className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MdLocalOffer /> Precio en promocion
+                    </div>
+                    <input
+                      type="number"
+                      {...register("promotionPrice", {
+                        required: "Campo obligatorio",
+                      })}
+                      placeholder="$0.00"
+                      className="w-96 rounded-sm text-black p-1"
+                    />
+                  </label>
+                  <label className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <MdOutlineLocalOffer /> Descripcion en promocion
+                    </div>
+                    <input
+                      type="text"
+                      {...register("promotionDescription", {
+                        required: "Campo obligatorio",
+                      })}
+                      placeholder="Descripcion"
+                      className="w-96 rounded-sm text-black p-1"
+                    />
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
           <div className=" col-span-4 flex flex-col gap-8">
@@ -155,7 +228,7 @@ const CreateProduct = () => {
                     <option>Valor 2</option>
                   </select>
                 </label>
-              </div>
+              </div>{" "}
             </div>
             <div className="bg-darkThird p-4 rounded-md">
               <h2 className="text-3xl mb-4">Imagenes</h2>
@@ -176,6 +249,7 @@ const CreateProduct = () => {
                       <FaImage />
                     )}
                     <input
+                      {...register("image")}
                       type="file"
                       ref={fileInputRef}
                       className="hidden"
@@ -187,10 +261,23 @@ const CreateProduct = () => {
                   Imagen de Exibhicion
                   <button
                     className="text-darkPrimary flex text-4xl justify-center items-center border h-24 border-darkPrimary"
-                    id="portada"
+                    onClick={handleButtonClick}
                   >
-                    <FaImage />
-                    <input type="file" className="hidden"></input>
+                    {previewImage ? (
+                      <img
+                        src={previewImage}
+                        alt="Preview"
+                        className="h-24 border border-darkPrimary"
+                      />
+                    ) : (
+                      <FaImage />
+                    )}
+                    <input
+                      type="file"
+                      ref={fileInputRef}
+                      className="hidden"
+                      onChange={handleFileChange}
+                    ></input>
                   </button>
                 </label>
               </div>
