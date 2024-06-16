52d04486e92bdd7380e7a47c3a314b1e54304eb4
pedromolina17 <pedro.molinanoa@gmail.com>
2024-06-16 17:08:58 -0500

    Image-Description-type

---
+++ client/src/api/images-description.ts
@@ -0,0 +1,18 @@
+import { imageCover } from "@/types/image-cover.type";
+import { api } from "./axios.config";
+import { AxiosResponse } from "axios";
+const imageDescriptionById = async (id: number): Promise<imageCover> => {
+  try {
+    const { data }: AxiosResponse<imageCover> = await api.get(
+      `imageProductCover/${id}/get-all-img-cover`
+    );
+    return data;
+  } catch (error) {
+    console.error("Error al obtener el producto:", error);
+    throw new Error(
+      "Error al obtener el producto. Inténtalo de nuevo más tarde."
+    );
+  }
+};
+
+export { imageDescriptionById };
