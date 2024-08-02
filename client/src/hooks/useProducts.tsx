043a950fab377035d1fd62f8ed49bf0373dea55e
pedromolina17 <pedro.molinanoa@gmail.com>
2024-08-01 22:11:33 -0500

    Fix table

---
+++ client/src/hooks/useProducts.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import { getAllProducts } from "../api/products";
+
+export const useProduct = () => {
+  const queryClient = useQueryClient();
+
+  const useGetAllProducts = () =>
+    useQuery({
+      queryKey: ["products"],
+      queryFn: () => getAllProducts,
+    });
+
+  //   const useGetProductById = (id) =>
+  //     useQuery({
+  //       queryKey: ["products"],
+  //       queryFn: () => getProductById(id),
+  //     });
+
+  return {
+    // addProductMutation,
+    useGetAllProducts,
+    // useGetProductById,
+    // deleteProductMutation,
+    // updateProductMutation,
+  };
+};
